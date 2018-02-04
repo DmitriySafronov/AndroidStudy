9ccce0c5cba28fdfa7f460d3aa68de18a7a020b9
Dmitriy Safronov <zimniy@cyberbrain.pw>
2018-02-04 22:56:34 +0300

    Second activity temp fragments

--- app/src/main/java/pw/cyberbrain/androidstudy/BottomNavigationActivity.java
+++ app/src/main/java/pw/cyberbrain/androidstudy/BottomNavigationActivity.java
@@ -3,31 +3,45 @@ package pw.cyberbrain.androidstudy;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class BottomNavigationActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    // Fragments
+    Fragment redFragment;
+    Fragment greenFragment;
+    Fragment blueFragment;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            boolean mResult = true;
 
-                    return true;
-                case R.id.navigation_dashboard:
+            // Handle bottom navigation view item clicks here.
+            int id = item.getItemId();
+            String title = (String) item.getTitle();
 
-                    return true;
-                case R.id.navigation_notifications:
-
-                    return true;
+            if (id == R.id.navigation_home) {
+                fragmentTransaction.replace(R.id.content2, redFragment);
+            } else if (id == R.id.navigation_dashboard) {
+                fragmentTransaction.replace(R.id.content2, greenFragment);
+            } else if (id == R.id.navigation_notifications) {
+                fragmentTransaction.replace(R.id.content2, blueFragment);
+            } else {
+                mResult = false;
             }
-            return false;
+            //toolbar.setTitle(title);
+            fragmentTransaction.commit();
+            return mResult;
         }
     };
 
@@ -36,8 +50,16 @@ public class BottomNavigationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_navigation);
 
+        // new Fragments
+        redFragment = new RedFragment();
+        greenFragment = new GreenFragment();
+        blueFragment = new BlueFragment();
+
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        navigation.setSelectedItemId(R.id.navigation_home);
     }
 
 }
