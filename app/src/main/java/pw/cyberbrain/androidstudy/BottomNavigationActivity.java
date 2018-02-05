8091ab75900b6fc074233da56a1c669eabd5beac
Dmitriy Safronov <zimniy@cyberbrain.pw>
2018-02-05 21:26:55 +0300

    Second activity with settings menu

--- app/src/main/java/pw/cyberbrain/androidstudy/BottomNavigationActivity.java
+++ app/src/main/java/pw/cyberbrain/androidstudy/BottomNavigationActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 
 public class BottomNavigationActivity extends AppCompatActivity {
@@ -47,19 +48,41 @@ public class BottomNavigationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bottom_navigation);
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_bottom_navigation);
 
-        // new Fragments
-        redFragment = new RedFragment();
-        greenFragment = new GreenFragment();
-        blueFragment = new BlueFragment();
+            // new Fragments
+            redFragment = new RedFragment();
+            greenFragment = new GreenFragment();
+            blueFragment = new BlueFragment();
 
 
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+            BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+            navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        navigation.setSelectedItemId(R.id.navigation_home);
+            navigation.setSelectedItemId(R.id.navigation_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+           // Inflate the menu; this adds items to the action bar if it is present.
+           getMenuInflater().inflate(R.menu.options, menu);
+           return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+           // Handle action bar item clicks here. The action bar will
+           // automatically handle clicks on the Home/Up button, so long
+           // as you specify a parent activity in AndroidManifest.xml.
+           int id = item.getItemId();
+
+           //noinspection SimplifiableIfStatement
+           if (id == R.id.action_settings) {
+               return true;
+           }
+
+           return super.onOptionsItemSelected(item);
     }
 
 }
